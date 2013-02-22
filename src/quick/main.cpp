867d4e14127ba092d53ac96c267e88d93997ec6f
Gilbert Röhrbein <payload@lavabit.com>
2013-02-22 18:51:23 +0100

    [quick] cmd line argument 1 is image file to open

--- src/quick/main.cpp
+++ src/quick/main.cpp
@@ -25,13 +25,14 @@ int main(int argc, char *argv[])
     QQmlDebuggingEnabler qml_debug;
     QGuiApplication app(argc, argv);
 
+    QString filepath = app.arguments().value(1, "./why_are_you_reading_this.png");
     Comic comic;
-    if (QFile("why_are_you_reading_this.png").exists()) {
-        comic.load(QImage("why_are_you_reading_this.png"));
+    if (QFile(filepath).exists()) {
+        comic.load(QImage(filepath));
     }
 
     QtQuick2ApplicationViewer viewer;
-    viewer.engine()->addImageProvider("comicframeprovider", new ComicFrameProvider(comic));
+    viewer.engine()->addImageProvider("comicimageprovider", new ComicImageProvider(comic));
     QQmlContext* context = viewer.rootContext();
     context->setContextProperty("comic", &comic);
     viewer.setMainQmlFile(QStringLiteral("qml/quick/main.qml"));
