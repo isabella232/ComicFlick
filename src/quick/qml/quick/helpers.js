3b20b691d234ef3000d4c61de4f251d7f2d731b2
Gilbert Röhrbein <payload@lavabit.com>
2013-02-24 22:46:32 +0100

    [quick] added helpers.js with ubuntu colors

---
+++ src/quick/qml/quick/helpers.js
@@ -0,0 +1,15 @@
+.pragma library
+
+var color = {
+    ubuntuOrange: "#DD4814",
+    white: "#FFFFFF",
+    black: "#000000",
+    lightAubergine: "#77216F",
+    midAubergine: "#5E2750",
+    darkAubergine: "#2C001E",
+    warmGray: "#AEA79F",
+    coolGray: "#333333",
+    smallDotGray: "#AEA79F",
+    textGray: "#333333",
+    canonicalAubergine: "#772953",
+}
